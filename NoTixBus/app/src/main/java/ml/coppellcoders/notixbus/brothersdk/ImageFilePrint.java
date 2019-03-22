25a7e53d28419ebc5c1430531d4c091830da7379
Darshan Bhatta <bhatta.darshan26@gmail.com>
2019-03-21 19:08:16 -0500

    printing stuff (does not work)

---
+++ NoTixBus/app/src/main/java/ml/coppellcoders/notixbus/brothersdk/ImageFilePrint.java
@@ -0,0 +1,62 @@
+/**
+ * FilePrint for printing
+ *
+ * @author Brother Industries, Ltd.
+ * @version 2.2
+ */
+package ml.coppellcoders.notixbus.brothersdk;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.brother.ptouch.sdk.PrinterInfo.ErrorCode;
+
+import java.util.ArrayList;
+
+public class ImageFilePrint extends BasePrint {
+
+    private ArrayList<String> mImageFiles;
+
+    public ImageFilePrint(Context context, MsgHandle mHandle) {
+        super(context, mHandle);
+    }
+
+    /**
+     * set print data
+     */
+    public ArrayList<String> getFiles() {
+        return mImageFiles;
+    }
+
+    /**
+     * set print data
+     */
+    public void setFiles(ArrayList<String> files) {
+        mImageFiles = files;
+    }
+
+    /**
+     * do the particular print
+     */
+    @Override
+    protected void doPrint() {
+
+        int count = mImageFiles.size();
+
+        for (int i = 0; i < count; i++) {
+
+            String strFile = mImageFiles.get(i);
+
+           mPrintResult = mPrinter.printFile(strFile);
+
+            // if error, stop print next files
+            if (mPrintResult.errorCode != ErrorCode.ERROR_NONE) {
+                break;
+            }
+        }
+    }
+
+
+
+}
